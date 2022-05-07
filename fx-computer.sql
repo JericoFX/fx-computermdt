5bfb213d88fb5027fc29b6d401ea66e32051657e
JERICOFX <blackjordison@gmail.com>
2022-05-07 12:27:13 -0300

    Changed SQL to match new rows

--- fx-computer.sql
+++ fx-computer.sql
@@ -32,31 +32,31 @@ CREATE TABLE IF NOT EXISTS `fx_assignment` (
 -- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla qbcorevue.fx_reports
-CREATE TABLE if NOT EXISTS`fx_reports` (
-  `algo` INT(11) NOT NULL AUTO_INCREMENT,
-  `id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
-  `title` VARCHAR(20) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-  `name` VARCHAR(20) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-  `lastname` VARCHAR(20) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-  `citizenid` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-  `plate` VARCHAR(50) NULL DEFAULT '""' COLLATE 'utf8_general_ci',
-  `location` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-  `coords` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-  `observations` TEXT NULL DEFAULT '""' COLLATE 'utf8_general_ci',
-  `data` LONGTEXT NULL DEFAULT '{"polices":[],"fines":[],"evidences":[]}' COLLATE 'utf8mb4_bin',
-  `amount` SMALLINT(6) NOT NULL DEFAULT '0',
-  `type` VARCHAR(20) NOT NULL DEFAULT 'basic' COLLATE 'utf8_general_ci',
-  `taked` INT(1) NULL DEFAULT '0',
-  `callsign` VARCHAR(50) NULL DEFAULT '""' COLLATE 'utf8_general_ci',
-  `isvehicle` TINYINT(2) NULL DEFAULT '0',
-  `date` TIMESTAMP NULL DEFAULT NULL,
-  PRIMARY KEY (`algo`) USING BTREE,
-  INDEX `id` (`id`) USING BTREE,
-  INDEX `citizenid` (`citizenid`) USING BTREE
+CREATE TABLE `fx_reports` (
+	`algo` INT(11) NOT NULL AUTO_INCREMENT,
+	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`title` VARCHAR(20) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`name` VARCHAR(20) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`lastname` VARCHAR(20) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`citizenid` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`plate` VARCHAR(50) NULL DEFAULT '""' COLLATE 'utf8_general_ci',
+	`location` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`coords` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`observations` TEXT NULL DEFAULT '""' COLLATE 'utf8_general_ci',
+	`data` LONGTEXT NULL DEFAULT '{"polices":[],"fines":[],"evidences":[]}' COLLATE 'utf8mb4_bin',
+	`amount` SMALLINT(6) NOT NULL DEFAULT '0',
+	`type` VARCHAR(20) NOT NULL DEFAULT 'basic' COLLATE 'utf8_general_ci',
+	`taked` INT(1) NULL DEFAULT '0',
+	`callsign` VARCHAR(50) NULL DEFAULT '""' COLLATE 'utf8_general_ci',
+	`isvehicle` TINYINT(2) NULL DEFAULT '0',
+	`date` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	PRIMARY KEY (`algo`) USING BTREE,
+	INDEX `id` (`id`) USING BTREE,
+	INDEX `citizenid` (`citizenid`) USING BTREE
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=139
+AUTO_INCREMENT=230
 ;
 
 
